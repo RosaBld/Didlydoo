721ff6a818bc8ca450125c9fb8051244249299d4
Rosa <rosalie.boulard@gmail.com>
2023-10-13 17:47:35 +0200

    trying the post

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 import { getData } from "./script/getDatabase.js";
-import { form } from './script/add.js';
+import { eventButton } from './script/add.js';
 
 
 getData();
-form();
-=======
-import { add } from "./script/add.js";
-import { getData } from "./getDatabase.js";
->>>>>>> dev
+eventButton();
--- script/add.js
+++ script/add.js
@@ -1,40 +1,75 @@
-const form = document.createElement("form");
+const eventButton = document.getElementById("eventButton");
 
-const titleInput = document.createElement("input");
-titleInput.type = "text";
-form.appendChild(titleInput);
+const result=document.querySelector(".result");
 
-const dateInput = document.createElement("input");
-dateInput.type = "date";
-dateInput.setAttribute("multiple", "");
-form.appendChild(dateInput);
 
-const submitButton = document.createElement("button");
-submitButton.type = "submit";
-submitButton.textContent = "Submit";
-form.appendChild(submitButton);
 
-document.body.appendChild(form);
+eventButton.addEventListener("click", () => {
+  const form = document.createElement("form");
+  result.appendChild(form);
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const formData = new FormData(form);
-  const dates = formData.getAll("date");
-  const data = {
-    title: formData.get("title"),
-    dates: dates
-  };
-  fetch("http://localhost:3000/api/events", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  })
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
-  form.reset();
+  const titleLabel = document.createElement("label");
+  titleLabel.textContent = "Title:";
+  form.appendChild(titleLabel);
+
+  const titleInput = document.createElement("input");
+  titleInput.type = "text";
+  titleInput.name = "title";
+  form.appendChild(titleInput);
+
+  const authorLabel=document.createElement("label");
+  authorLabel.textContent="Author:";
+  form.appendChild(authorLabel);
+
+  const authorInput=document.createElement("input");
+  authorInput.type="text";
+  authorInput.name="author";
+  form.appendChild(authorInput);
+
+  const dateLabel = document.createElement("label");
+  dateLabel.textContent = "Dates:";
+  form.appendChild(dateLabel);
+
+  const dateInput = document.createElement("input");
+  dateInput.type = "date";
+  dateInput.name = "date";
+  form.appendChild(dateInput);
+
+  const submitButton = document.createElement("button");
+  submitButton.type = "submit";
+  submitButton.textContent = "Submit";
+  form.appendChild(submitButton);
+
+  document.body.appendChild(form);
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const data = {
+      title: formData.get("title"),
+      date: formData.get("date"),
+      author: formData.get("author")
+    };
+    console.log(data);
+    fetch("http://localhost:3000/api/events", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      form.reset();
+    })
+    .catch(error => console.error(error));
+  });
 });
 
-export { form };
+export { eventButton };
