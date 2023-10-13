d8cc4a7c010350a124eb3683761ee4c0e818dce4
Robpiot <robin.piot@outlook.com>
2023-10-13 15:26:09 +0200

    ready to merge

--- script/getDatabase.js
+++ script/getDatabase.js
@@ -2,6 +2,7 @@
 
 const getData = async () => {
   const response = await fetch("http://localhost:3000/api/events", {
+    // using get method to get data from database
     method: "GET",
     headers: {
       "Content-Type": "application/json",
