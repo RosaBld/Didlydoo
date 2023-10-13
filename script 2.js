c4b33d1a36956ba908f3d81a3e6d5ccc68bc4fcd
Rosa <rosalie.boulard@gmail.com>
2023-10-13 14:57:18 +0200

    changes

--- script 2.js
+++
@@ -1,5 +0,0 @@
-const main=document.querySelector('main');
-
-const card=document.createElement('div');
-card.classList.add('card');
-main.appendChild(card);
--- script.js
+++ script.js
@@ -1,11 +1,2 @@
-<<<<<<< HEAD
 import { add } from './script/add.js';
-=======
 import { getData } from "./getDatabase.js";
-
-const main = document.querySelector("main");
-
-const card = document.createElement("div");
-card.classList.add("card");
-main.appendChild(card);
->>>>>>> Robin
