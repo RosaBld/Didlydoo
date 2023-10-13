d3cb3507dbaf4c610457fb3bfff031d209cdb58a
Robpiot <robin.piot@outlook.com>
2023-10-13 14:57:44 +0200

    :3

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
-import { add } from './script/add.js';
-=======
+import { add } from "./script/add.js";
+
 import { getData } from "./getDatabase.js";
 
 const main = document.querySelector("main");
@@ -8,4 +7,3 @@ const main = document.querySelector("main");
 const card = document.createElement("div");
 card.classList.add("card");
 main.appendChild(card);
->>>>>>> Robin
