67ce0e33591c61f8b2a5cceca85bbe72c36ff837
Robpiot <robin.piot@outlook.com>
2023-10-15 20:31:01 +0200

    fixing the display twice around the evenements appendind

--- script/createExistingCard.js
+++ script/createExistingCard.js
@@ -6,36 +6,35 @@ function createExistingCard() {
   getData()
     .then((data) => {
       console.log(data);
-      if (Array.isArray(data)) {
-        data.forEach((element) => {
-          const divHeader = document.createElement("div");
-          const divDate = document.createElement("div");
-          const titleEvent = document.createElement("h3");
-          const eventDescription = document.createElement("p");
-          const eventAuthor = document.createElement("p");
-          console.log(element);
+      if (data && data.length > 0) {
+        const element = data[0];
+        const divHeader = document.createElement("div");
+        const divDate = document.createElement("div");
+        const titleEvent = document.createElement("h3");
+        const eventDescription = document.createElement("p");
+        const eventAuthor = document.createElement("p");
+        console.log(element);
 
-          cardContainer.appendChild(divHeader);
-          cardContainer.appendChild(divDate);
-          divHeader.appendChild(titleEvent);
-          titleEvent.textContent = element.title;
-          divHeader.appendChild(eventDescription);
-          eventDescription.textContent = element.description;
-          divHeader.appendChild(eventAuthor);
-          eventAuthor.textContent = element.author;
+        cardContainer.appendChild(divHeader);
+        cardContainer.appendChild(divDate);
+        divHeader.appendChild(titleEvent);
+        titleEvent.textContent = element.title;
+        divHeader.appendChild(eventDescription);
+        eventDescription.textContent = element.description;
+        divHeader.appendChild(eventAuthor);
+        eventAuthor.textContent = element.author;
 
-          for (let date = 0; date < 5; date++) {
-            const elementDate = element.dates[date * 8];
-            if (elementDate) {
-              divDate.appendChild(document.createElement("p"));
-              divDate.childNodes[date].textContent = elementDate;
-            }
+        for (let date = 0; date < 5; date++) {
+          const elementDate = element.dates[date * 4];
+          if (elementDate) {
+            divDate.appendChild(document.createElement("p"));
+            divDate.childNodes[date].textContent = elementDate;
           }
-        });
+        }
       }
     })
     .catch((error) => {
-      console.log(error);
+      // Handle error
     });
 }
 
