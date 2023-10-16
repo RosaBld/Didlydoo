48324eccb410393c8edfbb2362dec9bcb04591c1
Robpiot <robin.piot@outlook.com>
2023-10-16 14:13:47 +0200

    struggling on this fucking table ffs

--- script/createExistingCard.js
+++ script/createExistingCard.js
@@ -3,15 +3,19 @@ import { getData } from "./getDatabase.js";
 const cardContainer = document.getElementById("importingCardFromDb");
 
 function createExistingCard() {
-  getData()
-    .then((data) => {
-      console.log(data);
-      if (data && data.length > 0) {
-        const divHeader = document.createElement("div");
-        divHeader.classList.add("header");
-        cardContainer.appendChild(divHeader);
+  getData().then((data) => {
+    console.log(data);
+    if (data && data.length > 0) {
+      const divHeader = document.createElement("div");
+      divHeader.classList.add("header");
+      cardContainer.appendChild(divHeader);
 
-        data.forEach((element) => {
+      // Create attendeesName element
+      const attendeesName = document.createElement("p");
+      divHeader.appendChild(attendeesName);
+
+      data
+        .forEach((element) => {
           const divCard = document.createElement("div");
           divHeader.appendChild(divCard);
           divCard.classList.add("card");
@@ -24,33 +28,58 @@ function createExistingCard() {
           eventDescription.textContent = element.description;
           divCard.appendChild(eventAuthor);
           eventAuthor.textContent = element.author;
+          const thead = document.createElement("thead");
+          const tbody = document.createElement("tbody");
+          const trHeader = document.createElement("tr");
+          const tr = document.createElement("tr");
           const divDate = document.createElement("div");
           divCard.appendChild(divDate);
+          divDate.appendChild(thead);
+          thead.appendChild(trHeader);
+          divDate.appendChild(tbody);
+          tbody.appendChild(tr);
+
           divDate.classList.add("date");
           if (element.dates && element.dates.length > 0) {
-            element.dates.forEach((date) => {
-              const eventDate = document.createElement("p");
-              divDate.appendChild(eventDate);
-              eventDate.textContent = date.date;
+            element.dates.forEach((date, index) => {
+              const dateHeader = document.createElement("th");
+              trHeader.appendChild(dateHeader);
+              dateHeader.textContent = date.date;
+
               if (date.attendees && date.attendees.length > 0) {
-                date.attendees.forEach((attendees) => {
-                  const attendeesName = document.createElement("p");
-                  const attendeesValue = document.createElement("p");
-                  divDate.appendChild(attendeesName);
-                  divDate.appendChild(attendeesValue);
-                  attendeesName.textContent = attendees.name;
-                  attendeesValue.textContent = attendees.available;
+                date.attendees.forEach((attendee) => {
+                  if (index === 0) {
+                    // Set attendeesName text content
+                    const attendeesName = document.createElement("td");
+                    const attendeesValue = document.createElement("td");
+                    tr.appendChild(attendeesName);
+                    tr.appendChild(attendeesValue);
+                    attendeesName.textContent = attendee.name;
+                    attendeesValue.textContent = attendee.available;
+                  }
+                  const attendeesValue = document.createElement("td");
+                  tbody.appendChild(attendeesValue);
+                  if (attendee.available === null) {
+                    attendee.available = "X";
+                    attendeesValue.textContent = attendee.available;
+                  } else if (attendee.available === true) {
+                    attendee.available = "V";
+                    attendeesValue.textContent = attendee.available;
+                  } else if (attendee.available === false) {
+                    attendee.available = "X";
+                    attendeesValue.textContent = attendee.available;
+                  }
                 });
               }
             });
           }
+        })
+        .catch((error) => {
+          // Handle error
+          console.log(error);
         });
-      }
-    })
-    .catch((error) => {
-      // Handle error
-      console.log(error);
-    });
+    }
+  });
 }
 
 export { createExistingCard };
