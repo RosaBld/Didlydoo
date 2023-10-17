a21a2573fe6a5c5ae6b47544d9ddae5ba4ff2436
alexandreVDW <vandewielealexandre@gmail.com>
2023-10-17 10:48:20 +0200

    function edit

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ import { getData } from "./script/getDatabase.js";
 import { addElement } from "./script/add.js";
 import { createExistingCard } from "/script/createExistingCard.js";
 
+
 getData();
 addElement();
 createExistingCard();
